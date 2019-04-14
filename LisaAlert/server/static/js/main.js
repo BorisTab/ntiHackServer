5f96ec83fa4e9c27734e85c760d17aaf0081c4b5
BorisTab <boristab23@gmail.com>
2019-04-14 18:15:56 +0300

    submit form for selecting people

--- LisaAlert/server/static/js/main.js
+++ LisaAlert/server/static/js/main.js
@@ -8,20 +8,24 @@ $(document).ready(() => {
     e.preventDefault();
     $('.menu').toggleClass('menu_active');
     $('.menu-btn').toggleClass('menu-btn_active');
-  })
+  });
 
-  $(".tab_item").not(":first").hide();
-  $(".wrapper1 .tab").click(function() {
-    $(".wrapper1 .tab").removeClass("active").eq($(this).index()).addClass("active");
-    $(".tab_item").hide().eq($(this).index()).fadeIn()
-  }).eq(0).addClass("active");
+  $('.tab_item').not('first').hide();
+  $('.wrapper1 .tab').click(function() {
+    $('.wrapper1 .tab').removeClass('active')
+        .eq($(this).index()).addClass('active');
+    $('.tab_item').hide().eq($(this).index()).fadeIn();
+  }).eq(0).addClass('active');
 
   $.ajax({
     type: 'GET',
     url: '/person/iswaiting',
   }).done((data) => {
     data.map((person) => {
-      $('.persons').html(`<input type="radio" value="${person}">`);
+      $('.people').html(`<label for="${person}">
+            <input id="${person}" type="checkbox">
+            ${person}
+          </label>`);
     });
     lastDataUsers = data;
   });
@@ -33,10 +37,32 @@ const getUsers = () => {
     type: 'GET',
     url: '',
   }).done((data) => {
-    if(data !== lastData) {
+    if (data !== lastData) {
       data.map((person) => {
-        $('.persons').html(`<input type="radio" value="${person}">`);
+        $('.people').html(`<label for="${person}">
+              <input id="${person}" type="checkbox">
+              ${person}
+            </label>`);
       });
     }
   });
 };
+
+$('.makeGroup').submit(() => {
+  const checkedPeople = {};
+  $(this).find('input').each(() => {
+    const person = $(this).prop('id');
+    if ($(this).prop('checked')) {
+      checkedPeople[`${person}`] = true;
+    }
+  });
+  $.ajax({
+    method: 'POST',
+    url: '',
+    data: checkedPeople,
+  }).done((data) => {
+    data.map((person) => {
+      $(`#${person}`).css('display', 'none');
+    });
+  });
+});
