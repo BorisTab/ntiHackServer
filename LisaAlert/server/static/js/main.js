641a5aa42f12f3e956dfeb74b5555cd78e1b5f06
timakon <1910tim@mail.ru>
2019-04-15 19:47:53 +0300

    debug menu

--- LisaAlert/server/static/js/main.js
+++ LisaAlert/server/static/js/main.js
@@ -5,15 +5,13 @@ $(document).ready(() => {
     e.preventDefault();
     $('.menu').toggleClass('menu_active');
     $('.menu-btn').toggleClass('menu-btn_active');
-  });
+  })
 
-  $('.tab_item').not('first').hide();
-  $('.wrapper1 .tab').click(function() {
-    $('.wrapper1 .tab').removeClass('active')
-        .eq($(this).index()).addClass('active');
-    $('.tab_item').hide().eq($(this).index()).fadeIn();
-  }).eq(0).addClass('active');
-
+  $(".tab_item").not(":first").hide();
+  $(".wrapper1 .tab").click(function() {
+    $(".wrapper1 .tab").removeClass("active").eq($(this).index()).addClass("active");
+    $(".tab_item").hide().eq($(this).index()).fadeIn()
+  }).eq(0).addClass("active");
   $.ajax({
     type: 'GET',
     url: '/person/is_waiting/',
